315e2aed2df208923178925b47de222143fe6696
Safar Safarov <Safarov.safar@hotmail.com>
2022-08-30 23:54:35 +0500

    ✨

--- Fasty-SwiftUI/ContentView.swift
+++ Fasty-SwiftUI/ContentView.swift
@@ -8,6 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var fastingManager = FastingManager()
+    
+    var title: String {
+        switch fastingManager.fastingState {
+        case .notStarted:
+            return "Let's get started!"
+            
+        case .fasting:
+            return "You are now fasting"
+        case .feeding:
+            return "You are now feeding"
+        }
+    }
     var body: some View {
         ZStack {
             // MARK: Background
@@ -19,55 +32,65 @@ struct ContentView: View {
     }
     
     var content: some View {
-        VStack(spacing: 40) {
-            // MARK: Progress Ring
-            Text("Let's get started!")
-                .font(.headline)
-                .foregroundColor(Color(#colorLiteral(red: 0.3663273156, green: 0.5047162175, blue: 0.9721121192, alpha: 1)))
-            
-            
-            // MARK: Fasting Plan
-                .fontWeight(.bold)
-                .padding(.horizontal, 24)
-                .padding()
-            
-            // MARK: Progress Ring
-            
-            ProgressRing()
-            
-            HStack(spacing: 60) {
-                // MARK: Start Time
+        ZStack {
+            VStack(spacing: 40) {
+                // MARK: Title
+                Text("Let's get started!")
+                    .font(.headline)
+                    .foregroundColor(Color(#colorLiteral(red: 0.3663273156, green: 0.5047162175, blue: 0.9721121192, alpha: 1)))
                 
-                VStack(spacing: 6) {
-                    Text("Start")
-                        .opacity(0.7)
-                    
-                    Text(Date(), format: .dateTime.weekday().hour().minute().second())
-                        .fontWeight(.bold)
-                }
-                
-                // MARK: End Time
-                
-                VStack(spacing: 5) {
-                    Text("End")
-                        .opacity(0.7)
-                    
-                    Text(Date().addingTimeInterval(16), format: .dateTime.weekday().hour().minute().second())
-                        .fontWeight(.bold)
-                }
-            }
-            
-            Button {
-                
-            } label: {
-                Text("Start fasting")
-                    .font(.title3)
-                    .fontWeight(.bold)
+                // MARK: Fasting Plan
+                Text(fastingManager.fastingPlan.rawValue)
+                    .fontWeight(.semibold)
                     .padding(.horizontal, 24)
                     .padding(.vertical, 8)
                     .background(.thinMaterial)
                     .cornerRadius(20)
+                
+                Spacer()
             }
+            
+            
+            VStack(spacing: 40) {
+                // MARK: Progress Ring
+                
+                ProgressRing()
+                    .environmentObject(fastingManager)
+                HStack(spacing: 60) {
+                    // MARK: Start Time
+                    
+                    VStack(spacing: 5) {
+                        Text(fastingManager.fastingState == .notStarted ? "Start" : "Started")
+                            .opacity(0.7)
+                        
+                        Text(Date(), format: .dateTime.weekday().hour().minute().second())
+                            .fontWeight(.bold)
+                    }
+                    
+                    // MARK: End Time
+                    
+                    VStack(spacing: 5) {
+                        Text(fastingManager.fastingState == .notStarted ? "End" : "Ends")
+                            .opacity(0.7)
+                        
+                        Text(fastingManager.endTime, format: .dateTime.weekday().hour().minute().second())
+                            .fontWeight(.bold)
+                    }
+                }
+                
+                Button {
+                    fastingManager.toggleFastingState()
+                } label: {
+                    Text(fastingManager.fastingState == .fasting ? "End fast" : "Start fasting")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 24)
+                        .padding(.vertical, 8)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                }
+            }
+            .padding()
         }
         .foregroundColor(.white)
     }
--- Fasty-SwiftUI/ProgressRing.swift
+++ Fasty-SwiftUI/ProgressRing.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct ProgressRing: View {
-    @State var progress = 0.5
+    @EnvironmentObject var fastingManager: FastingManager
+    
+    let timer = Timer
+        .publish(every: 1, on: .main, in: .common)
+        .autoconnect()
+    
     var body: some View {
         ZStack {
             // MARK: Placeholder Ring
@@ -21,37 +26,59 @@ struct ProgressRing: View {
             // MARK: Colored Ring
             
             Circle()
-                .trim(from:0.0, to: min(progress, 1.0))
+                .trim(from:0.0, to: min(fastingManager.progress, 1.0))
                 .stroke(AngularGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.3663273156, green: 0.5047162175, blue: 0.9721121192, alpha: 1)), Color(#colorLiteral(red: 0.942648828, green: 0.4157198966, blue: 0.6704799533, alpha: 1)), Color(#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)), Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.3701431751, green: 0.5008467436, blue: 0.9525166154, alpha: 1))]), center: .center), style: StrokeStyle(lineWidth: 15.0, lineCap: .round, lineJoin: .round))
                 .rotationEffect((Angle(degrees: 270)))
-                .animation(.easeInOut(duration: 1.0), value: progress)
+                .animation(.easeInOut(duration: 1.0), value: fastingManager.progress)
             
             VStack(spacing: 30) {
-                // MARK: Elapsed Time
-                VStack(spacing: 5) {
-                    Text("Elapsed Time")
-                        .opacity(0.7)
+                if fastingManager.fastingState == .notStarted {
+                    // MARK: Upcoming Fast
+                    VStack(spacing: 5) {
+                        Text("Upcoming fast")
+                            .opacity(0.7)
+                        
+                        Text("\(fastingManager.fastingPlan.fastingPeriod.formatted()) Hours")
+                            .font(.title)
+                            .fontWeight(.bold)
+                    }
+                } else {
+                    // MARK: Elapsed Time
+                    VStack(spacing: 5) {
+                        Text("Elapsed Time (\(fastingManager.progress.formatted(.percent))")
+                            .opacity(0.7)
+                        
+                        Text(fastingManager.startTime, style: .timer)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                    }
+                    .padding(.top)
                     
-                    Text("0:00")
-                        .font(.title2)
-                        .fontWeight(.bold)
+                    // MARK: Remaining Time
+                    VStack(spacing: 5 ) {
+                        if !fastingManager.elapsed {
+                            Text("Remaining Time (\((1 - fastingManager.progress).formatted(.percent))")
+                                .opacity(0.7)
+                        } else {
+                            Text("Extra Time")
+                                .opacity(0.7)
+                        }
+                        
+                        Text(fastingManager.endTime, style: .timer)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                    }
                 }
-                .padding(.top)
-                
-                // MARK: Remaining Time
-                VStack(spacing: 5 ) {
-                    Text("Remaining Time")
-                        .opacity(0.7)
-                    Text("0:00")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                }
+               
             }
         }
         .frame(width: 250, height: 250)
         .padding()
-        .onAppear {
-            progress = 1
+//        .onAppear {
+//            progress = 1
+//        }
+        .onReceive(timer) { _ in
+            fastingManager.track()
         }
     }
 }
@@ -59,5 +86,6 @@ struct ProgressRing: View {
 struct ProgressRing_Previews: PreviewProvider {
     static var previews: some View {
         ProgressRing()
+            .environmentObject(FastingManager())
     }
 }
