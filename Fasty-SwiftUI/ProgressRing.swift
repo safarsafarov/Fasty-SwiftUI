c57c897772709d9b361373de1bf4b0c15e1175dc
Safar Safarov <Safarov.safar@hotmail.com>
2022-08-30 17:18:09 +0500

    ✨

--- Fasty-SwiftUI/ProgressRing.swift
+++ Fasty-SwiftUI/ProgressRing.swift
@@ -25,9 +25,33 @@ struct ProgressRing: View {
                 .stroke(AngularGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.3663273156, green: 0.5047162175, blue: 0.9721121192, alpha: 1)), Color(#colorLiteral(red: 0.942648828, green: 0.4157198966, blue: 0.6704799533, alpha: 1)), Color(#colorLiteral(red: 0.8164353967, green: 0.6586706042, blue: 0.8116279244, alpha: 1)), Color(#colorLiteral(red: 0.5596573353, green: 0.8194586635, blue: 0.843054235, alpha: 1)), Color(#colorLiteral(red: 0.3701431751, green: 0.5008467436, blue: 0.9525166154, alpha: 1))]), center: .center), style: StrokeStyle(lineWidth: 15.0, lineCap: .round, lineJoin: .round))
                 .rotationEffect((Angle(degrees: 270)))
                 .animation(.easeInOut(duration: 1.0), value: progress)
+            
+            VStack(spacing: 30) {
+                // MARK: Elapsed Time
+                VStack(spacing: 5) {
+                    Text("Elapsed Time")
+                        .opacity(0.7)
+                    
+                    Text("0:00")
+                        .font(.title2)
+                        .fontWeight(.bold
+                }
+                .padding(.top)
+                // MARK: Remaining Time
+                VStack(spacing: 5 ) {
+                    Text("Remaining Time")
+                        .opacity(0.7)
+                    Text("0:00")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                }
+            }
         }
         .frame(width: 250, height: 250)
         .padding()
+        .onAppear {
+            progress = 1
+        }
     }
 }
 
